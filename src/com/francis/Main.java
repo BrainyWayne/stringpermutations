b2e511f3ad0b88aafbda1195746808b75c907f50
Francis Eshun <eshunfrancisboateng@gmail.com>
2020-07-30 19:16:55 +0000

    Optimized

--- src/com/francis/Main.java
+++ src/com/francis/Main.java
@@ -6,20 +6,27 @@ public class Main {
     public static void main(String[] args) {
 	    Scanner scanner = new Scanner(System.in);
 	    printOut("Enter the string to find perms\n");
-	    returnPerms(scanner.next());
+	    returnPerms(scanner.next(), "");
     }
 
-    static void returnPerms(String string){
-        char[] stringarr = string.toCharArray();
-        for(int i = 0; i < stringarr.length; i++){
-            printOut(String.valueOf(stringarr[i]));
-            for(int j = 0; j < stringarr.length; j++){
-                if(i != j){
-                    printOut(String.valueOf(stringarr[j]));
-                }
-                
-            }
-            printOut(" ");
+    static void returnPerms(String str, String ans){
+        if (str.length() == 0) {
+            System.out.print(ans + " ");
+            return;
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+
+            // ith character of str
+            char ch = str.charAt(i);
+
+            // Rest of the string after excluding
+            // the ith character
+            String ros = str.substring(0, i) +
+                    str.substring(i + 1);
+
+            // Recurvise call
+            returnPerms(ros, ans + ch);
         }
     }
 
